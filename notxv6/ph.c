431baea69908c40ecc5607d00f48d7622fcfcdf1
PinkCave <1228937201@qq.com>
2023-07-13 15:47:51 +0800

    labs6

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t mutex[NBUCKET];
 
 double
 now()
@@ -40,13 +41,13 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
+  pthread_mutex_lock(&mutex[i]);
   if(e){
     // update the existing key.
     e->value = value;
@@ -54,7 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&mutex[i]);
 }
 
 static struct entry*
